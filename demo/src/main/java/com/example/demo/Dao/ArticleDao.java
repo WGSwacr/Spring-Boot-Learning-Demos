ab1fc322541d9cec6f0dfc44d0e5055895011865
Allen Tan <wacr.thj@qq.com>
2023-05-29 15:27:12 +0800

    add articledao

---
+++ demo/src/main/java/com/example/demo/Dao/ArticleDao.java
@@ -0,0 +1,16 @@
+package com.example.demo.Dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.example.demo.Model.T_Article;
+
+public interface ArticleDao {
+
+    int addArticle(T_Article t_Article);
+    int updateArticle(T_Article t_Article);
+    int deleteArticle(T_Article t_Article);
+    T_Article findArticleById(int id);
+
+    List<Map<String, Object>> listArticle();
+}
---
+++ demo/src/main/java/com/example/demo/Dao/ArticleDaoImplement.java
@@ -0,0 +1,48 @@
+package com.example.demo.Dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.Model.T_Article;
+
+@Repository
+public class ArticleDaoImplement implements ArticleDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public int addArticle(T_Article t_Article) {
+        return jdbcTemplate.update("INSERT INTO t_article(author, content, type) values(?, ?, ?)",
+        t_Article.getAuthor(), t_Article.getContent(), t_Article.getType());
+    }
+
+    @Override
+    public int updateArticle(T_Article t_Article) {
+        return jdbcTemplate.update("UPDATE t_article SET content=? WHERE id=?",
+        t_Article.getContent(), t_Article.getId());
+    }
+
+    @Override
+    public int deleteArticle(T_Article t_Article) {
+        return jdbcTemplate.update("DELETE FROM t_article WHERE id=?",
+        t_Article.getId());
+    }
+
+    @Override
+    public T_Article findArticleById(int id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM t_article WHERE id=?",
+        new Object[]{id}, new BeanPropertyRowMapper<T_Article>(T_Article.class));
+    }
+
+    @Override
+    public List<Map<String, Object>> listArticle() {
+        return jdbcTemplate.queryForList("SELECT * FROM t_article");
+    }
+    
+}
